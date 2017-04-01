dd3ab9e91368642b600a48c0e63764419c91de89
lukebelliveau <lbellive@thoughtworks.com>
2017-04-01 16:51:53 -0500

    using CDN instead of webpack for tachyon || Revert "reconfigured webpack for tachyons styling"
    This reverts commit 3f6b0d733136f31e633fc003dc986395fda893a9.

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,22 @@
 const path = require('path');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  entry: {
-    app: './src/renderDOM.jsx',
-    tachyons: 'tachyons/css/tachyons.css',
-    index: './src/styles/index.css'
-  },
+  entry: './src/renderDOM.jsx',
   output: {
     path: path.resolve('dist'),
-    publicPath: '/',
-    filename: '[name].js',
+    filename: 'app.bundle.js',
   },
   resolve: {
-    extensions: ['.css', '.js', '.jsx'],
+    extensions: [".js", ".jsx"],
   },
-  plugins: [
-    new ExtractTextPlugin({ filename: '[name].css', disable: false, allChunks: true }),
-  ],
   module: {
     loaders: [
-      {
-        test: /\.css$/,
-        loader: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader' })
-      },
       { test: /\.js$/,
         loader: 'babel-loader',
-        exclude: /node_modules/
-      },
+        exclude: /node_modules/ },
       { test: /\.jsx$/,
         loader: 'babel-loader',
-        exclude: /node_modules/
-      },
+        exclude: /node_modules/ },
     ]
   },
-  devServer: {
-    port: 3000,
-    historyApiFallback: {
-      index: 'index.html'
-    }
-  }
 };
