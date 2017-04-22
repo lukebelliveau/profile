7c6907f2c735d3ad2f18a10c5970a8360cb7d0f5
lukebelliveau <luke.belliveau@gmail.com>
2017-04-22 14:30:00 -0500

    added dumb header template

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 
 const headerStyle = {
   display: 'grid',
-  gridTemplateColumns: 'repeat(5, 1fr)',
-  color: 'red',
+  gridTemplateColumns: 'repeat(2, 1fr)',
+  textAlign: 'center',
+  color: 'white',
+  backgroundColor: 'black',
+  padding: '10px 10px',
 };
 
 const Header = () => (
   <div style={ headerStyle }>
-    <div>Section 1</div>
-    <div>Section 2</div>
-    <div>Section 3</div>
-    <div>Section 4</div>
-    <div>Section 5</div>
+    <div>resume</div>
+    <div>games</div>
   </div>
 );
 
